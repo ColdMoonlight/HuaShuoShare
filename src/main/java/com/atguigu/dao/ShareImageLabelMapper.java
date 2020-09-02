bb8c3f6c73e72869fa5630f10b1632e753e0b1c6
Administrator <Administrator@PC-202007311130>
2020-09-02 15:55:15 +0800

    add imagelable

--- src/main/java/com/atguigu/dao/ShareImageLabelMapper.java
+++ src/main/java/com/atguigu/dao/ShareImageLabelMapper.java
@@ -3,15 +3,17 @@ package com.atguigu.dao;
 import com.atguigu.bean.ShareImageLabel;
 
 public interface ShareImageLabelMapper {
-    int deleteByPrimaryKey(Integer imageLabelId);
 
     int insert(ShareImageLabel record);
 
-    int insertSelective(ShareImageLabel record);
-
     ShareImageLabel selectByPrimaryKey(Integer imageLabelId);
 
-    int updateByPrimaryKeySelective(ShareImageLabel record);
-
     int updateByPrimaryKey(ShareImageLabel record);
+    
+    int insertSelective(ShareImageLabel record);
+    
+    int deleteByPrimaryKey(Integer imageLabelId);
+    
+    int updateByPrimaryKeySelective(ShareImageLabel record);
+    
 }
