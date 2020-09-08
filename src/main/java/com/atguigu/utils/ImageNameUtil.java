3d7396de9ff7063f8595886c395904ae86f90c20
Administrator <Administrator@PC-202007311130>
2020-09-08 16:17:09 +0800

    update

--- src/main/java/com/atguigu/utils/ImageNameUtil.java
+++ src/main/java/com/atguigu/utils/ImageNameUtil.java
@@ -3,7 +3,8 @@ package com.atguigu.utils;
 public class ImageNameUtil {
 
 	public static String getImageInfofilename(String typeName, String parentIdStr, String parentname) {
-		String newfilename = parentIdStr +parentname+typeName;
+		//String newfilename = parentIdStr +parentname+typeName;
+		String newfilename = parentIdStr + parentname + typeName;
 		System.out.println(newfilename);
 		return newfilename;
 	}
