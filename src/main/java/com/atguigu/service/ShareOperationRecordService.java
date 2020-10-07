6895555606745502613d4a84a81c31bd45ae97d3
Administrator <Administrator@PC-202007311130>
2020-10-07 10:52:43 +0800

    update

---
+++ src/main/java/com/atguigu/service/ShareOperationRecordService.java
@@ -0,0 +1,59 @@
+package com.atguigu.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.atguigu.bean.ShareOperationRecord;
+import com.atguigu.dao.ShareOperationRecordMapper;
+
+@Service
+public class ShareOperationRecordService {
+	
+	@Autowired
+	ShareOperationRecordMapper shareOperationRecordMapper;
+	
+	/**
+	 * @author Shinelon
+	 * @param shareOperationRecord
+	 * @exception add方法
+	 * */
+	public int insertSelective(ShareOperationRecord shareOperationRecord) {
+		int intReslut = shareOperationRecordMapper.insertSelective(shareOperationRecord);
+		return intReslut;
+	}
+	
+	/**
+	 * @author Shinelon
+	 * @param shareOperationRecord
+	 * @exception	update方法
+	 * */
+	public int updateByPrimaryKeySelective(ShareOperationRecord shareOperationRecord) {
+		int intReslut = shareOperationRecordMapper.updateByPrimaryKeySelective(shareOperationRecord);
+		return intReslut;
+	}
+	
+	/**
+	 * @author Shinelon
+	 * @param shareOperationRecord
+	 * @exception	update方法
+	 * */
+	public void deleteByPrimaryKey(Integer VideoLabelId) {
+		shareOperationRecordMapper.deleteByPrimaryKey(VideoLabelId);
+	}
+
+	public List<ShareOperationRecord> selectShareOperationRecordByDate(ShareOperationRecord shareOperationRecord) {
+		List<ShareOperationRecord> ShareOperationRecordList = shareOperationRecordMapper.selectShareOperationRecordByDate(shareOperationRecord);
+		return ShareOperationRecordList;
+	}
+	
+//	/**
+//	 * @author Shinelon
+//	 * @param ShareOperationRecord
+//	 * @exception	selectShareOperationRecordlistByPid方法
+//	 * */
+//	public List<ShareOperationRecord> selectShareOperationRecordlistByPid(ShareOperationRecord shareOperationRecord) {
+//		List<ShareOperationRecord> ShareOperationRecordList = shareOperationRecordMapper.selectShareOperationRecordlistByPid(shareOperationRecord);
+//		return ShareOperationRecordList;
+//	}
+
+}
